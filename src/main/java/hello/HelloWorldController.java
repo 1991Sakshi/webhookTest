6e19fb552ae381e28f02fb1435caca621941c76c
1991Sakshi <herokuSak@gmail.com>
2017-06-27 07:50:30 -0400

    Add files via upload

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -6,23 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-
-
 @Controller
 @RequestMapping("/webhook")
 public class HelloWorldController {
 
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
-     return new WebhookResponse("Hello!", "Text " + obj);
-    }//webhookResponse
+
+        System.out.println(obj);
+
+        return new WebhookResponse("Hello! " + obj, "Text " + obj);
+    }
 }
