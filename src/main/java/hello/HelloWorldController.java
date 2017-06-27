8cd04c54e24bf4e5c8fb3a6320a073a042bdabe3
SakshiSS <githubsak@gmail.com>
2017-06-26 20:58:09 -0400

    Add files via upload

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -6,15 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.*;
+
 @Controller
 @RequestMapping("/webhook")
 public class HelloWorldController {
 
     @RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
-
-        System.out.println(obj);
-        String n="Sakshi";
-        return new WebhookResponse("Hello! " + n, "Text " + obj);
-    }
+    public @ResponseBody WebhookResponse webhook(@RequestBody String obj) throws JsonParseException, JsonMappingException, IOException{
+    	ObjectMapper objectMapper = new ObjectMapper();
+    	byte[] mapData = obj.getBytes();
+    	Map<String,String> myMap = new HashMap<String,String>();
+ 	myMap = objectMapper.readValue(mapData, //HashMap.class);
+    return new WebhookResponse("Hello! Sakshi" , "Text " + obj);
+   }//webhookResponse
 }
